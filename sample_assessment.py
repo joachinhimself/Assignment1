aca1a30a1d298a25962f321d2ba2def8924613bd
joachinhimself <88300749+joachinhimself@users.noreply.github.com>
2021-08-11 17:07:33 +0800

    Add files via upload

---
+++ sample_assessment.py
@@ -0,0 +1,11 @@
+# Question 1
+def add():
+# Question 1
+#Modify the add function such that it takes two integers and returns the sum of the two integers.
+a = int(input())
+b = int(input())
+c = a + b
+def add():
+  return(c)
+print(c)
+  
