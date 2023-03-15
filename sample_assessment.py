a2ea85bb536da91f94b9a116f87f78c838c1774d
Duruiheme Obinna Joachin <88300749+joachinhimself@users.noreply.github.com>
2023-03-15 17:12:56 +0800

    Update sample_assessment.py

--- sample_assessment.py
+++ sample_assessment.py
@@ -3,10 +3,8 @@
 # Question 1
 #Modify the add function such that it takes two integers and returns the sum of the two integers.
 def add(a,b):
+  a = int(input('Assign number for "a" between 1 to 9:   '))
+  b = int(input('Assign number for "b" between 1 to 9:   '))
   return a + b
-a = int(input('Assign number for "a" between 1 to 9   '))
-b = int(input('Assign number for "b" between 1 to 9   '))
-c = add(a,b)
-print(c)
-
+add(a,b)
   
